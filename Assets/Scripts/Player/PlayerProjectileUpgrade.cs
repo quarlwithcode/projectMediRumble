a563a8fb5f937ef6914abe55f71a46f790080f68
Vincent Quarles <soulcaught@gmail.com>
2016-08-17 13:29:23 -0400

    Comp Switch
    vnq

---
+++ Assets/Scripts/Player/PlayerProjectileUpgrade.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProjectileUpgrade : MonoBehaviour {
+
+	private PlayerLevelController playerLevel;
+	public bool activateUpgrade;
+	public int numUpgrades = 1;
+	// Use this for initialization
+	void Start () {
+		playerLevel = GetComponent<PlayerLevelController> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (playerLevel.currentLevel == 3) {
+			
+		}
+	}
+
+	public void Upgrade(int level){
+		if (level == 3) {
+			
+		}
+	}
+}
---
+++ Assets/Scripts/Player/ProjectileSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSwitcher : MonoBehaviour {
+
+	private PlayerLevelController playerLevel;
+	public bool activateUpgrade;
+	// Use this for initialization
+	void Start () {
+		playerLevel = GetComponent<PlayerLevelController> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playerLeve)
+	}
+}
