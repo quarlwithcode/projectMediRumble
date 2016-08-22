25e6b833120be35aa553109287dadd7b45690680
Vincent Quarles <soulcaught@gmail.com>
2016-08-22 18:16:03 -0400

    Added Arrow, Halfway done with Air Balloon
    vnq

---
+++ Assets/Scripts/Enemy/EnemyAirBalloonMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAirBalloonMovement : EnemyMovement {
+
+	protected override void Start ()
+	{
+		attackStop = GameObject.Find("AirBalloonStopPoint").GetComponent<Transform>();
+		base.Start ();
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyDoubleShoot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDoubleShoot : EnemyShoot {
+
+	protected override void AutoAttack(){
+		GameObject projectileClone;
+		projectileClone = Instantiate (projectile, transform.GetChild (0).position, transform.GetChild (0).rotation) as GameObject;
+		projectileClone.transform.GetChild(1).GetComponent<EnemyProjectile> ().SetDamage(attackDamage);
+		projectileClone.transform.GetChild(1).GetComponent<Rigidbody2D> ().AddForce (new Vector2 (.7F, 1) * shotSpeed,ForceMode2D.Impulse);
+		projectileClone = Instantiate (projectile, transform.GetChild (1).position, transform.GetChild (1).rotation) as GameObject;
+		projectileClone.transform.GetChild(1).GetComponent<EnemyProjectile> ().SetDamage(attackDamage);
+		projectileClone.transform.GetChild(1).GetComponent<Rigidbody2D> ().AddForce (new Vector2 (.7F, 1) * shotSpeed,ForceMode2D.Impulse);
+	}
+
+}
